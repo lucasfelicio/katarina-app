bebad5c2a0403a920a045de90ca32190cfaae17f
Lucas Felicio <felicio.lucas@outlook.com>
2020-06-25 17:13:19 -0400

    1.0.8

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL: `http://192.168.100.4:3333`
 });
-    
+
+api.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 export default api;
